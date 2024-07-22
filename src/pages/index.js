38717a997931b361390188e68ba04cfe04e8b223
Jonkellys <jonke2331@gmail.com>
2024-07-22 19:26:28 -0400

    Added theme change function

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,21 @@
+import { useEffect, useState } from "react";
+
 export default function Home() {
-	return <div className="h-screen w-screen bg-pink-300"></div>;
+	const [theme, setTheme] = useState("light");
+	const changeTheme = () => {
+		setTheme(theme === "light" ? "dark" : "light");
+	};
+	useEffect(() => {
+		if (theme === "dark") {
+			document.querySelector("html")?.classList.add("dark");
+		} else {
+			document.querySelector("html")?.classList.remove("dark");
+		}
+	}, [theme]);
+
+	return (
+		<div className="h-screen w-screen bg-pink-300 dark:bg-purple-300">
+			<button onClick={changeTheme}>Change Theme</button>
+		</div>
+	);
 }
