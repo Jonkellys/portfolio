5ffa8d2240dc582530698bfc2ac0f1165b0e1687
Jonkellys <jonke2331@gmail.com>
2024-07-23 23:50:23 -0400

    Added language change function

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,23 @@
 import "@/styles/globals.css";
+import { useRouter } from 'next/router'
+import { IntlProvider } from 'react-intl'
+import en from '../../i18n/en.json'
+import es from '../../i18n/es.json';
+
+const messages = {
+	en, es
+};
+
+function getDirection(locale) {
+	return "ltr";
+}
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+	const {locale} = useRouter();
+	
+  	return (
+	  	<IntlProvider locale={locale} messages={messages [locale]}>
+  			<Component {...pageProps} dir={getDirection(locale)} />
+  		</IntlProvider>
+  	);
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
+import { useRouter } from "next/router";
+import { FormattedMessage, useIntl } from "react-intl";
+import Link from "next/link";
 
 export default function Home() {
 	const [theme, setTheme] = useState("light");
@@ -13,9 +16,28 @@ export default function Home() {
 		}
 	}, [theme]);
 
+	const { locales } = useRouter();
+	const intl = useIntl();
+	
+	const title = intl.formatMessage({
+		id: "page.home.head.meta.description",
+	});
+
 	return (
 		<div className="h-screen w-screen bg-pink-300 dark:bg-purple-300">
 			<button onClick={changeTheme}>Change Theme</button>
+			<br></br>
+			<p>{title}</p>
+			<h1><FormattedMessage id="page.home.title" /></h1>
+			<p><FormattedMessage id="page.home.description" /></p>
+			<br></br>
+			<div>
+			  {[...locales].sort().map((locale) => (
+			    <Link key={locale} href="/" locale={locale}>
+			      <div>{locale}</div>
+			    </Link>
+			  ))}
+			</div>
 		</div>
 	);
 }
