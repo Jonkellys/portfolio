0fca69af01a9d4c5e1b36b1a8b9e79c4725f6e32
Jonkellys <jonke2331@gmail.com>
2024-07-26 22:07:38 -0400

    Added hello section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,12 @@
-// import { useEffect, useState } from "react";
-// import { useRouter } from "next/router";
-// import { FormattedMessage, useIntl } from "react-intl";
-// import Link from "next/link";
 import Hello from "./sections/Hello";
 import Header from "./sections/Header";
 import OptionsMenu from "./sections/OptionsMenu";
 
 export default function Home() {
-	// const { locales } = useRouter();
-	// const intl = useIntl();
-
-	// const title = intl.formatMessage({
-	// 	id: "page.home.head.meta.description",
-	// });
-
-	// return (
-	// 	<div className="h-screen w-screen bg-pink-300 dark:bg-purple-300">
-	// 		<button onClick={changeTheme}>Change Theme</button>
-	// 		<br></br>
-	// 		<p>{title}</p>
-	// 		<h1><FormattedMessage id="page.home.title" /></h1>
-	// 		<p><FormattedMessage id="page.home.description" /></p>
-	// 		<br></br>
-	// 		<div>
-	// 		  {[...locales].sort().map((locale) => (
-	// 		    <Link key={locale} href="/" locale={locale}>
-	// 		      <div>{locale}</div>
-	// 		    </Link>
-	// 		  ))}
-	// 		</div>
-	// 	</div>
-	// );
-
 	return (
 		<div className="w-screen h-auto bg-gray-50 dark:bg-gray-800 text-gray-800 dark:text-gray-50">
 			<Header />
-			<div className="w-full p-8">
+			<div className="w-full px-8 pb-8 flex flex-col items-center">
 				<OptionsMenu />
 				<Hello />
 			</div>
--- src/pages/sections/Hello.jsx
+++ src/pages/sections/Hello.jsx
@@ -1,7 +1,31 @@
+import Image from "next/image";
+import image1 from "../../images/image1.png";
+import { FormattedMessage } from "react-intl";
+
 export default function Hello() {
 	return (
-		<div className="w-full h-5/6 p-8 mt-14">
-			<h1 className="text-4xl">Hello</h1>
+		<div
+			id="hello"
+			className="w-11/12 h-6/6 p-8 mt-14 flex flex-col bg-purple-400 rounded-b-2xl"
+		>
+			<div className="w-full h-full flex justify-center">
+				<div className="w-2/3 h-full p-12">
+					<h1 className="text-6xl mb-6">
+						<FormattedMessage id="hello.greet" />
+						<br />
+						Jonkellys Maestre
+					</h1>
+					<p className="text-2xl">
+						<FormattedMessage id="hello.description" />
+					</p>
+				</div>
+				<div className="w-1/3 h-full flex justify-center align-end">
+					<Image src={image1} alt="Hello" className="size-3/4" />
+				</div>
+			</div>
+			<div className="justify-self-center text-center mt-14 text-xl">
+				<FormattedMessage id="hello.learn-more" />
+			</div>
 		</div>
 	);
 }
